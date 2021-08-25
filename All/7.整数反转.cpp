b5fa76a45ac01e0cf76306540369b69acb12633e
liu15388282385 <1436255653@qq.com>
2021-08-25 16:47:07 +0800

    master

---
+++ All/7.整数反转.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+
+        while (x != 0) {
+            // 判断整数溢出
+            if (res < INT_MIN /10 || res > INT_MAX / 10) {
+                return 0;
+            }
+            // 每次取末尾数字
+            int tmp = x % 10;
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+        
+        return res;
+    }
+};
+// @lc code=end
+
