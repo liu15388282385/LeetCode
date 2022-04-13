406011c9aa70a75195e23d3ed27697a63888530c
liu15388282385 <1436255653@qq.com>
2022-04-13 23:40:01 +0800

    q2-begin

--- All/1.两数之和.cpp
+++ All/1.两数之和.cpp
@@ -3,12 +3,25 @@
  *
  * [1] 两数之和
  */
-
+#include <map>
+#include <vector>
 // @lc code=start
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::map<int, int> hashtable;
+        std::vector<int> res;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end())
+            {
+                res.push_back(it->second);
+                res.push_back(i);
+            }
+            hashtable[nums[i]] = i;
+        }
+        return res;
     }
 };
 // @lc code=end
