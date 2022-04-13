ef057bb8666068b27660ed6659710622296fc4bf
liu15388282385 <1436255653@qq.com>
2022-04-13 23:42:43 +0800

    check

--- All/2.两数相加.cpp
+++ All/2.两数相加.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-
+#include <iostream>
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -16,16 +16,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    struct ListNode {
+struct ListNode {
         int val;
         ListNode *next;
         ListNode() : val(0), next(nullptr) {}
         ListNode(int x) : val(x), next(nullptr) {}
         ListNode(int x, ListNode *next) : val(x), next(next) {}
-    };
-
+};
+class Solution {
+public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
     }
