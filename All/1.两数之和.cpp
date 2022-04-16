8e246944ad5051a495199cc9ad2456ac61c51d6e
liu15388282385 <1436255653@qq.com>
2022-04-16 14:51:06 +0800

    correction

--- All/1.两数之和.cpp
+++ All/1.两数之和.cpp
@@ -9,21 +9,12 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-<<<<<<< HEAD
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::map<int ,int> hashtable;
-        std::vector<int> result;
-        for (int i = 0; i < nums.size(); i++) {
-=======
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> hashtable;
         vector<int> res;
-        for (int i = 0; i < nums.size(); i++)
-        {
->>>>>>> deputy
+        for (int i = 0; i < nums.size(); i++){
             auto it = hashtable.find(target - nums[i]);
-            if (it != hashtable.end())
-            {
+            if (it != hashtable.end()){
                 res.push_back(it->second);
                 res.push_back(i);
                 break;
